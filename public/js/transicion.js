fe3ae56e5e039133ffb78d12ea58409c72b6d517
JesúsVidalLuyo <jesus.luyoc@vallegrande.edu.pe>
2024-10-30 21:58:42 -0500

    Personal

--- public/js/transicion.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const elements = document.querySelectorAll('.slide-in');
-    elements.forEach((el, index) => {
-        setTimeout(() => {
-            el.classList.add('visible');
-        }, index * 100);
-    });
-});
